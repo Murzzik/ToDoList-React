61f474453569ea2753dfcc85b4abb15208783f64
Murzzik <mcalexstar@gmail.com>
2022-04-01 12:28:07 +0300

    Updates

---
+++ precache-manifest.d45dfc5e1a7f4703192fd1ab78805e04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bc79436ecd40ec90ec2080caec69ff8",
+    "url": "/ToDoList-React/index.html"
+  },
+  {
+    "revision": "15d430d8d348787754d2",
+    "url": "/ToDoList-React/static/css/main.70f552d2.chunk.css"
+  },
+  {
+    "revision": "741cae798b52d4d67f65",
+    "url": "/ToDoList-React/static/js/2.fa7efa35.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/ToDoList-React/static/js/2.fa7efa35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15d430d8d348787754d2",
+    "url": "/ToDoList-React/static/js/main.41f7cfec.chunk.js"
+  },
+  {
+    "revision": "9a3f888bcbf1427183f1",
+    "url": "/ToDoList-React/static/js/runtime-main.5bd76bdf.js"
+  }
+]);
